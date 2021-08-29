65811272fb83aec42f73326b64e174785ea77cfc
t1mron <timron.work@gmail>
2021-08-29 12:32:51 +0300

    add

--- .config/polybar/scripts/mic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-volume=$(awk -F"[][]" '/Front Left:/ { print $2 }' <(amixer sget Capture))
-
-if [[ "$volume" == "0%" ]]; then
-  echo "%{F#cccccc}"
-else
-  echo ""
-fi
--- .config/sxhkd/scripts/mic-toggle.sh
+++
@@ -1,9 +0,0 @@
-###---MICROPHONE---###
-MIC=$(amixer sget Capture | awk -F"[][]" '/Front Left:/ {print $2}')
-
-case "$MIC" in
-  25%)
-    amixer set Capture 0% ;;
-  0%)
-    amixer set Capture 25% ;;
-esac
