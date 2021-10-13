23a9cce408521a08510e7932861bc0e3f2a4846b
Tim Ron <timron.work@gmail.com>
2021-10-13 16:04:19 +0300

    Update arch.sh

--- script/arch.sh
+++ script/arch.sh
@@ -111,7 +111,7 @@ cd $HOME/git/yay && makepkg -si
 
 packagelist=(
   # Window manager
-  rxvt-unicode-truecolor-wide-glyphs lf-bin alternating-layouts-git
+  polybar rxvt-unicode-truecolor-wide-glyphs lf-bin alternating-layouts-git
   # Network
   iwgtk
   # Coreboot
